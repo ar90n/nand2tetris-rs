dab30b8f3ae6ab40b0753663aef05f927b531d19
Masahiro Wada <argon.argon.argon@gmail.com>
2022-02-14 16:14:25 +0000

    refactor vm

--- vm/src/code.rs
+++ vm/src/code.rs
@@ -1,7 +1,6 @@
 use rand::{distributions::Alphanumeric, Rng};
 
 use super::command::{Command as VmCommand, Segment};
-use assembler::code::assemble;
 use assembler::command::{Command, Comp, Dest, Jump};
 
 pub fn parse(lines: &[String]) -> anyhow::Result<Vec<VmCommand>> {
@@ -24,50 +23,48 @@ fn random_string(len: usize) -> String {
 }
 
 fn translate_vmcommand(vm_command: &VmCommand) -> Vec<Command> {
-    fn translate_unary_command(f: impl Fn() -> Vec<Command>) -> Vec<Command> {
-        let mut result = Vec::new();
-        result.extend(vec![
-            Command::ASymbol("SP".to_string()),
-            Command::C(Dest::A, Comp::M, Jump::None),
-            Command::C(Dest::A, Comp::A_MINUS_ONE, Jump::None),
-            Command::C(Dest::D, Comp::M, Jump::None),
-        ]);
-        result.extend(f());
-        result.extend(vec![
-            Command::ASymbol("SP".to_string()),
-            Command::C(Dest::A, Comp::M, Jump::None),
-            Command::C(Dest::A, Comp::A_MINUS_ONE, Jump::None),
-            Command::C(Dest::M, Comp::D, Jump::None),
-        ]);
-        result
-    }
+    let push = [
+        Command::ASymbol("SP".to_string()),
+        Command::C(Dest::A, Comp::M, Jump::None),
+        Command::C(Dest::M, Comp::D, Jump::None),
+        Command::ASymbol("SP".to_string()),
+        Command::C(Dest::M, Comp::M_PLUS_ONE, Jump::None),
+    ];
 
-    fn translate_binary_command(f: impl Fn() -> Vec<Command>) -> Vec<Command> {
-        let mut result = Vec::new();
-        result.extend(vec![
-            Command::ASymbol("SP".to_string()),
-            Command::C(Dest::A, Comp::M, Jump::None),
-            Command::C(Dest::A, Comp::A_MINUS_ONE, Jump::None),
-            Command::C(Dest::D, Comp::M, Jump::None),
-            Command::C(Dest::A, Comp::A_MINUS_ONE, Jump::None),
-            Command::C(Dest::A, Comp::M, Jump::None),
-        ]);
-        result.extend(f());
-        result.extend(vec![
-            Command::ASymbol("SP".to_string()),
-            Command::C(Dest::A, Comp::M, Jump::None),
-            Command::C(Dest::A, Comp::A_MINUS_ONE, Jump::None),
-            Command::C(Dest::A, Comp::A_MINUS_ONE, Jump::None),
-            Command::C(Dest::M, Comp::D, Jump::None),
-            Command::C(Dest::D, Comp::A, Jump::None),
-            Command::C(Dest::D, Comp::D_PLUS_ONE, Jump::None),
-            Command::ASymbol("SP".to_string()),
-            Command::C(Dest::M, Comp::D, Jump::None),
-        ]);
-        result
-    }
+    let pop = [
+        Command::ASymbol("SP".to_string()),
+        Command::C(Dest::M, Comp::M_MINUS_ONE, Jump::None),
+        Command::ASymbol("SP".to_string()),
+        Command::C(Dest::A, Comp::M, Jump::None),
+        Command::C(Dest::A, Comp::M, Jump::None),
+    ];
 
-    fn translate_condition_command(jump: Jump) -> Vec<Command> {
+    let swap = [
+        Command::C(Dest::D, Comp::D_PLUS_A, Jump::None),
+        Command::C(Dest::A, Comp::D_MINUS_A, Jump::None),
+        Command::C(Dest::D, Comp::D_MINUS_A, Jump::None),
+    ];
+
+    let translate_unary_command = |c: Command| -> Vec<Command> {
+        let mut result = Vec::new();
+        result.extend(pop.clone());
+        result.push(Command::C(Dest::D, Comp::A, Jump::None));
+        result.extend([c]);
+        result.extend(push.clone());
+        result
+    };
+
+    let translate_binary_command = |cs: Vec<Command>| -> Vec<Command> {
+        let mut result = Vec::new();
+        result.extend(pop.clone());
+        result.push(Command::C(Dest::D, Comp::A, Jump::None));
+        result.extend(pop.clone());
+        result.extend(cs);
+        result.extend(push.clone());
+        result
+    };
+
+    fn assign_imm_if(jump: Jump, true_value: Comp, false_value: Comp) -> Vec<Command> {
         let l0 = random_string(12);
         let l1 = random_string(12);
         vec![
@@ -75,171 +72,98 @@ fn translate_vmcommand(vm_command: &VmCommand) -> Vec<Command> {
             Command::ASymbol(l0.clone()),
             Command::C(Dest::None, Comp::D, jump),
             Command::ASymbol(l1.clone()),
-            Command::C(Dest::D, Comp::ZERO, Jump::JMP),
+            Command::C(Dest::D, false_value, Jump::JMP),
             Command::L(l0.clone()),
-            Command::C(Dest::D, Comp::MINUS_ONE, Jump::None),
+            Command::C(Dest::D, true_value, Jump::None),
             Command::L(l1.clone()),
         ]
     }
 
-    fn push_d_reg() -> Vec<Command> {
+    let calc_address = |symbol_name: String, index: u16| -> Vec<Command> {
         vec![
-            Command::ASymbol("SP".to_string()),
-            Command::C(Dest::A, Comp::M, Jump::None),
-            Command::C(Dest::M, Comp::D, Jump::None),
-            Command::C(Dest::D, Comp::A_PLUS_ONE, Jump::None),
-            Command::ASymbol("SP".to_string()),
-            Command::C(Dest::M, Comp::D, Jump::None),
-        ]
-    }
-
-    fn pop_d_reg() -> Vec<Command> {
-        vec![
-            Command::ASymbol("SP".to_string()),
-            Command::C(Dest::A, Comp::M, Jump::None),
-            Command::C(Dest::D, Comp::A_MINUS_ONE, Jump::None),
-            Command::ASymbol("SP".to_string()),
-            Command::C(Dest::M, Comp::D, Jump::None),
-            Command::C(Dest::A, Comp::D, Jump::None),
-            Command::C(Dest::D, Comp::M, Jump::None),
-        ]
-    }
-
-    fn load_segment(symbol_name: String, index: u16, offset: u16) -> Vec<Command> {
-        let mut commands = vec![
             Command::AImm(index),
             Command::C(Dest::D, Comp::A, Jump::None),
-            //Command::ASymbol(symbol_name),
-            (if offset == 0 || offset == 0xffff {
-                Command::ASymbol(symbol_name.clone())
-            } else {
-                Command::AImm(offset)
-            }),
-            (if offset == 0 {
-                Command::C(Dest::A, Comp::M, Jump::None)
-            } else if offset == 0xffff {
-                Command::ASymbol(symbol_name.clone())
-            } else {
-                Command::AImm(offset)
-            }),
-            Command::C(Dest::A, Comp::D_PLUS_A, Jump::None),
-            Command::C(Dest::D, Comp::M, Jump::None),
-        ];
-        commands.extend(push_d_reg());
+            Command::ASymbol(symbol_name.clone()),
+            Command::C(Dest::A, Comp::D_PLUS_M, Jump::None),
+        ]
+    };
+
+    let load_segment = |calc_address_commands: Vec<Command>| -> Vec<Command> {
+        let mut commands = calc_address_commands;
+        commands.push(Command::C(Dest::D, Comp::M, Jump::None));
+        commands.extend(push.clone());
         commands
-    }
-    fn store_segment(symbol_name: String, index: u16, offset: u16) -> Vec<Command> {
-        let mut commands = vec![
-            // D <- symbol + index
-            Command::AImm(index),
-            Command::C(Dest::D, Comp::A, Jump::None),
-            (if offset == 0 || offset == 0xffff {
-                Command::ASymbol(symbol_name.clone())
-            } else {
-                Command::AImm(offset)
-            }),
-            (if offset == 0 {
-                Command::C(Dest::A, Comp::M, Jump::None)
-            } else if offset == 0xffff {
-                Command::ASymbol(symbol_name.clone())
-            } else {
-                Command::AImm(offset)
-            }),
-            Command::C(Dest::D, Comp::D_PLUS_A, Jump::None),
-            // next <- symbol + index
-            Command::ASymbol("SP".to_string()),
-            Command::C(Dest::A, Comp::M, Jump::None),
-            Command::C(Dest::M, Comp::D, Jump::None),
-            // D <- top
-            Command::C(Dest::A, Comp::A_MINUS_ONE, Jump::None),
-            Command::C(Dest::D, Comp::M, Jump::None),
-            // A <- symbol + index
-            Command::ASymbol("SP".to_string()),
-            Command::C(Dest::A, Comp::M, Jump::None),
-            Command::C(Dest::A, Comp::M, Jump::None),
-            // symbol + index <- top
-            Command::C(Dest::M, Comp::D, Jump::None),
-            // A <- SP
-            Command::ASymbol("SP".to_string()),
-            Command::C(Dest::A, Comp::M, Jump::None),
-            // D <- SP - 1
-            Command::C(Dest::D, Comp::A_MINUS_ONE, Jump::None),
-            // A <- SP
-            Command::ASymbol("SP".to_string()),
-            Command::C(Dest::M, Comp::D, Jump::None),
-        ];
+    };
+
+    let store_segment = |calc_address_commands: Vec<Command>| -> Vec<Command> {
+        let mut commands = calc_address_commands;
+        commands.push(Command::C(Dest::D, Comp::A, Jump::None));
+        commands.extend(pop.clone());
+        commands.extend(swap.clone());
+        commands.push(Command::C(Dest::M, Comp::D, Jump::None));
         commands
-    }
+    };
 
     match vm_command {
         VmCommand::Push(segment, index) => match segment {
-            Segment::Argument => load_segment("ARG".to_string(), *index, 0),
-            Segment::Local => load_segment("LCL".to_string(), *index, 0),
-            Segment::This => load_segment("THIS".to_string(), *index, 0),
-            Segment::That => load_segment("THAT".to_string(), *index, 0),
-            Segment::Pointer => load_segment("SP".to_string(), *index, 3),
-            Segment::Temp => load_segment("SP".to_string(), *index, 5),
-            Segment::Static => {
-                let symbol_name = format!("__STATIC.{}", index);
-                let mut commands = load_segment(symbol_name, 0, 0xffff);
-                commands
-            }
+            Segment::Argument => load_segment(calc_address("ARG".to_string(), *index)),
+            Segment::Local => load_segment(calc_address("LCL".to_string(), *index)),
+            Segment::This => load_segment(calc_address("THIS".to_string(), *index)),
+            Segment::That => load_segment(calc_address("THAT".to_string(), *index)),
+            Segment::Pointer => load_segment(vec![Command::AImm(*index + 3)]),
+            Segment::Temp => load_segment(vec![Command::AImm(*index + 5)]),
+            Segment::Static => load_segment(vec![Command::ASymbol(format!("__STATIC.{}", index))]),
             Segment::Constant => {
                 let mut commands = vec![
                     Command::AImm(*index),
                     Command::C(Dest::D, Comp::A, Jump::None),
                 ];
-                commands.extend(push_d_reg());
+                commands.extend(push.clone());
                 commands
             }
-            _ => vec![],
+            _ => panic!("unsupported segment for push: {:?}", segment),
         },
         VmCommand::Pop(segment, index) => match segment {
-            Segment::Argument => store_segment("ARG".to_string(), *index, 0),
-            Segment::Local => store_segment("LCL".to_string(), *index, 0),
-            Segment::This => store_segment("THIS".to_string(), *index, 0),
-            Segment::That => store_segment("THAT".to_string(), *index, 0),
-            Segment::Pointer => store_segment("SP".to_string(), *index, 3),
-            Segment::Temp => store_segment("SP".to_string(), *index, 5),
-            Segment::Static => {
-                let symbol_name = format!("__STATIC.{}", index);
-                let mut commands = store_segment(symbol_name, 0, 0xffff);
-                commands
-            }
+            Segment::Argument => store_segment(calc_address("ARG".to_string(), *index)),
+            Segment::Local => store_segment(calc_address("LCL".to_string(), *index)),
+            Segment::This => store_segment(calc_address("THIS".to_string(), *index)),
+            Segment::That => store_segment(calc_address("THAT".to_string(), *index)),
+            Segment::Pointer => store_segment(vec![Command::AImm(*index + 3)]),
+            Segment::Temp => store_segment(vec![Command::AImm(*index + 5)]),
+            Segment::Static => store_segment(vec![Command::ASymbol(format!("__STATIC.{}", index))]),
             Segment::Constant => {
                 vec![
                     Command::ASymbol("SP".to_string()),
-                    Command::C(Dest::A, Comp::M, Jump::None),
-                    Command::C(Dest::D, Comp::A_MINUS_ONE, Jump::None),
-                    Command::ASymbol("SP".to_string()),
-                    Command::C(Dest::M, Comp::D, Jump::None),
+                    Command::C(Dest::M, Comp::M_MINUS_ONE, Jump::None),
                 ]
             }
-            _ => vec![],
+            _ => panic!("unsupported segment for pop: {:?}", segment),
         },
 
         VmCommand::Add => {
-            translate_binary_command(|| vec![Command::C(Dest::D, Comp::D_PLUS_A, Jump::None)])
+            translate_binary_command(vec![Command::C(Dest::D, Comp::D_PLUS_A, Jump::None)])
         }
         VmCommand::Sub => {
-            translate_binary_command(|| vec![Command::C(Dest::D, Comp::A_MINUS_D, Jump::None)])
+            translate_binary_command(vec![Command::C(Dest::D, Comp::A_MINUS_D, Jump::None)])
         }
-        VmCommand::Neg => {
-            translate_unary_command(|| vec![Command::C(Dest::D, Comp::MINUS_D, Jump::None)])
+        VmCommand::Neg => translate_unary_command(Command::C(Dest::D, Comp::MINUS_D, Jump::None)),
+        VmCommand::Eq => {
+            translate_binary_command(assign_imm_if(Jump::JEQ, Comp::MINUS_ONE, Comp::ZERO))
         }
-        VmCommand::Eq => translate_binary_command(|| translate_condition_command(Jump::JEQ)),
-        VmCommand::Gt => translate_binary_command(|| translate_condition_command(Jump::JGT)),
-        VmCommand::Lt => translate_binary_command(|| translate_condition_command(Jump::JLT)),
+        VmCommand::Gt => {
+            translate_binary_command(assign_imm_if(Jump::JGT, Comp::MINUS_ONE, Comp::ZERO))
+        }
+        VmCommand::Lt => {
+            translate_binary_command(assign_imm_if(Jump::JLT, Comp::MINUS_ONE, Comp::ZERO))
+        }
         VmCommand::And => {
-            translate_binary_command(|| vec![Command::C(Dest::D, Comp::D_AND_A, Jump::None)])
+            translate_binary_command(vec![Command::C(Dest::D, Comp::D_AND_A, Jump::None)])
         }
         VmCommand::Or => {
-            translate_binary_command(|| vec![Command::C(Dest::D, Comp::D_OR_A, Jump::None)])
+            translate_binary_command(vec![Command::C(Dest::D, Comp::D_OR_A, Jump::None)])
         }
-        VmCommand::Not => {
-            translate_unary_command(|| vec![Command::C(Dest::D, Comp::INV_D, Jump::None)])
-        }
-        _ => panic!("aaaa"),
+        VmCommand::Not => translate_unary_command(Command::C(Dest::D, Comp::INV_D, Jump::None)),
+        _ => panic!("unsupported command"),
     }
 }
 
